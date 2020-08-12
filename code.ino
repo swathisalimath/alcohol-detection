b39693cca996219e601bdaf08f95837e17a9fc73
swathisalimath <64915991+swathisalimath@users.noreply.github.com>
2020-08-12 21:46:02 +0530

    Create code.ino

---
+++ code.ino
@@ -0,0 +1,62 @@
+#include <LiquidCrystal.h>
+int val=0;
+const int ledred=8;
+const int ledgreen=7;
+const int buzzer=9;
+const int motorPin1  = 10;  // Pin Conected to Ia1
+const int motorPin2  = 13;  // Pin Conected to Ia2
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup()
+            {
+              Serial.begin(9600);
+            pinMode(ledred,OUTPUT);
+            pinMode(ledgreen,OUTPUT);
+           // pinMode(motorpin,OUTPUT);
+            pinMode(buzzer,OUTPUT);
+            pinMode(motorPin1, OUTPUT);
+            pinMode(motorPin2, OUTPUT);
+             // set up the LCD's number of columns and rows: 
+            lcd.begin(16, 2);
+             // Print a message to the LCD.
+             lcd.print("alcohol content");
+            
+
+}
+
+void loop()
+{  
+
+      val=analogRead(A0);
+      Serial.print("alcohol content");
+      Serial.println(val);
+       lcd.setCursor(0, 1);
+       lcd.print(val);
+      if(val>600)
+      {
+      digitalWrite(ledgreen,LOW);
+      digitalWrite(ledred,LOW);
+      digitalWrite(ledred,HIGH);
+      digitalWrite(motorPin1,LOW);
+ 
+       digitalWrite(motorPin2,LOW);
+       digitalWrite(buzzer,HIGH);
+  }
+else
+  { 
+      digitalWrite(ledred,LOW);
+      
+      digitalWrite(ledgreen,LOW);
+      
+      digitalWrite(ledgreen,HIGH);
+      digitalWrite(buzzer,LOW);
+    
+    digitalWrite(motorPin1, HIGH);  
+    digitalWrite(motorPin2, LOW);   
+    delay(1);
+        
+  }
+
+
+    delay(100); 
+}
